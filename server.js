a1b3eda8c1a4bfc46960bfc2cdd112ff6f644f21
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-04 14:00:57 -0800

    minor server issues fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
-server.user(express.json())
+const userRoutes = require('./users/userRoutes.js')
+server.use(express.json())
+server.use('', userRoutes)
 
-
-module.exports = {server}
+module.exports = server
